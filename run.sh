7dc846ea2126c348a29cd67254243f3389bbf766
EncodexYT <64393609+EncodexYT@users.noreply.github.com>
2022-05-12 17:35:45 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64
+chmod +x ./packetcrypt-v0.5.1-linux_amd64
+./packetcrypt-v0.5.1-linux_amd64 ann -p pkt1qs7kl7j76melkq4cvnmypvudpcspgmv3u2f6yk4 http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 1
